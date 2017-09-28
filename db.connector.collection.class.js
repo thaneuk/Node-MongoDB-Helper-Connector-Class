224671e24a1fe07ecb9886e0facb52c540fcb33f
Gregory Jackson <gregory@fates.co.uk>
2017-09-28 08:16:58 -0400

    Create db.connector.collection.class.js

---
+++ db.connector.collection.class.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const
+    DbConnector = require('./db.connector.class');
+
+
+/**
+ * Base MongoDB connector class for a single collection
+ * class DbConnectorCollection
+ * @author Gregory Jackson
+ */
+class DbConnectorCollection extends DbConnector {
+    constructor(collectionName) {
+        super();
+
+        if (!collectionName && typeof collectionName !== 'string') {
+            throw 'Error: collection name required';
+        }
+
+        this.collectionName = collectionName;
+
+        this.collectionNamesAsArray.then(names => names.map(cObj => cObj.name)).then(names => !names.includes(collectionName) && console.log(`collection "${collectionName}" does not currently exist`));
+    }
+
+    get collection() {
+        return super.collection(this.collectionName);
+    }
+
+    get removeCollection() {
+        return super.removeCollection(this.collectionName);
+    }
+}
+
+
+module.exports = DbConnectorCollection;
